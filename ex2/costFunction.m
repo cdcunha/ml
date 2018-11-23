c67070fe0d183e1267a1d2d236f4d9be0d2b37ad
cdcunha <cdcunha@gmail.com>
2018-11-23 16:17:21 -0200

    Últimas alterações

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -20,13 +20,13 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-h = sigmoid(X*theta);
+h = sigmoid(X * theta);
 
 % J = (1/m)*sum(-y .* log(h) - (1 - y) .* log(1-h));
 %J = (1/m)*(-y'* log(h) - (1 - y)'* log(1-h));
 %grad = (1/m)*X'*(h - y);
 
-J = (-1/m) * sum(y .* log(h) + (1 - y) .* log(1 - h)); 
+J = 1/m * sum(-y .* log(h) - (1 - y) .* log(1 - h)); 
 
 grad = (1/m) * X' * (h - y);
 
--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,15 +17,14 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-h = sigmoid(X*theta);
+h = sigmoid(X * theta);
 
-shift_theta = theta(2:size(theta));
-theta_reg = [0;shift_theta];
+J = (-1/m * sum((y.*log(h)) + ((1-y) .* log(1-h)))) + ((lambda/(2*m)) * (sum(power(theta,2))-power(theta(1),2)));
 
-J = -1/m * sum(y' .* log(h)  + (1 - y)' .* log(1 - h)) + (lambda/(2*m)) * theta_reg' * theta_reg;
-
-grad = (1/m) * (X' * (h-y) + lambda);
-
+grad(1) = (1/m) .* sum((h-y) .* X(:,1));
+for i = 2:numel(theta)
+  grad(i) = ((1/m) .* sum((h-y) .* X(:,i))) + (lambda/m)*theta(i);
+endfor
 
 % =============================================================
 
